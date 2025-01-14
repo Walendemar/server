23b46fb1af831017848b8088b587f9f273431fd4
Walendemar <windowtothespace@gmail.com>
2025-01-14 17:53:45 +0300

    Init server

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  if (req.url === "/" && req.method === "GET") {
+    res.end("Hello, world!");
+  } else if (req.url === "/about" && req.method === "GET") {
+    res.end("About page");
+  } else {
+    res.writeHead(404, { "Content-type": "text/plain" });
+    res.end("Page not found");
+  }
+});
+
+server.listen(PORT, () => {
+  console.log("Server running");
+});
